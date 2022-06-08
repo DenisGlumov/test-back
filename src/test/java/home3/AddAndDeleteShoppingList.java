8f093cb23f0f8525bcc312396b80b72b9ae6fb68
DenisGlumov <den.glumov@gmail.com>
2022-06-08 23:05:17 +0700

    Правки

--- src/test/java/home3/AddAndDeleteShoppingList.java
+++ src/test/java/home3/AddAndDeleteShoppingList.java
@@ -1,12 +1,15 @@
 package home3;
 
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+
 import static io.restassured.RestAssured.given;
 
-public class AddAndDeleteShoppingList extends AbstractTest{
+public class AddAndDeleteShoppingList extends AbstractTest {
 
     @BeforeAll
     static void assuredTest() {
@@ -15,21 +18,34 @@ public class AddAndDeleteShoppingList extends AbstractTest{
     }
 
     @Test
-    void postAddAndDeleteShoppingList () {
+    void postAddAndDeleteShoppingList() {
+
         given()
-                .contentType("application/json")
+                //.contentType("application/x-www-form-urlencoded")
                 .queryParam("apiKey", getApikey())
-                .queryParam("hash", "e11170b8cbd2d74102651cb967fa28e5")
-                .queryParam("username", "den.glumov")
+                .queryParam("hash", "4cff46e48bc0b34aa67e096741b1339d9f05d2c4")
+                .queryParam("username", "88c3f9fa-a337-4a33-9057-ec74b43a0ac9")
                 .body("{\n" +
                         "\t\"item\": \"TestGlimov01\",\n" +
                         "\t\"aisle\": \"Baking\",\n" +
                         "\t\"parse\": true\n" +
                         "}")
                 .when()
-                .post(getBaseUrl()+"/mealplanner/den.glumov/shopping-list/items")
+                .post(getBaseUrl() + "/mealplanner/88c3f9fa-a337-4a33-9057-ec74b43a0ac9/shopping-list/items")
                 .then()
                 .statusCode(200);
+
+
+        String id = given()
+                .queryParam("apiKey", getApikey())
+                .queryParam("hash", "4cff46e48bc0b34aa67e096741b1339d9f05d2c4")
+                .queryParam("username", "88c3f9fa-a337-4a33-9057-ec74b43a0ac9")
+                .when()
+                .get(getBaseUrl() + "/mealplanner/88c3f9fa-a337-4a33-9057-ec74b43a0ac9/shopping-list")
+                .body()
+                .jsonPath()
+                .get("id")
+                .toString();
 
 
     }
